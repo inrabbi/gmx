b12498661d2e3d54f5afbd63e0821eb3595fc78f
rabbicraft <rabbicraft@outlook.com>
2024-06-18 13:52:30 -0700

    hi

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'robhh001@gmail.com'  # Your Gmail email address
-app.config['MAIL_PASSWORD'] = 'dvziyvhsvijuhtht'  # Your Gmail password or App Password
+app.config['MAIL_PASSWORD'] = 'oyefeskysffycsci'  # Your Gmail password or App Password
 
 mail = Mail(app)
 

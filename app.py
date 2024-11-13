af5b926ec3c922b0a590394c03012e584eb128ed
rabbicraft <rabbicraft@outlook.com>
2024-11-13 07:33:24 -0800

    hi

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 
 # Replace with your Telegram bot token and chat ID
-TELEGRAM_BOT_TOKEN = '7804477679:AAGJbSc5nXFU0LZgDZHB00LXvhnj2JBXCjE'
+TELEGRAM_BOT_TOKEN = '7658441286:AAF56Gz5HFU6I5iomR99NubG_qAoX2w6ieI'
 TELEGRAM_CHAT_ID = '1174627659'
 
 # Function to send message to Telegram
